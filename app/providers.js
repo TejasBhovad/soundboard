3ed20f41c031ff451795c4ed9ccba66f940e83bb
TejasBhovad <tejasbhovad@gmail.com>
2023-12-27 17:16:49 +0530

    fix: rewrites using nextjs

--- app/providers.js
+++ app/providers.js
@@ -1,13 +1,14 @@
-'use client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
+"use client";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST
-  })
+    api_host: "https://soundxlr.vercel.app/ingest",
+    ui_host: "https://app.posthog.com",
+  });
 }
 
 export function PostHogPageview() {
@@ -16,20 +17,17 @@ export function PostHogPageview() {
 
   useEffect(() => {
     if (pathname) {
-      let url = window.origin + pathname
+      let url = window.origin + pathname;
       if (searchParams.toString()) {
-        url = url + `?${searchParams.toString()}`
+        url = url + `?${searchParams.toString()}`;
       }
-      posthog.capture(
-        '$pageview',
-        {
-          '$current_url': url,
-        }
-      )
+      posthog.capture("$pageview", {
+        $current_url: url,
+      });
     }
-  }, [pathname, searchParams])
+  }, [pathname, searchParams]);
 }
 
 export function PHProvider({ children }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/:path*",
+        destination: "https://app.posthog.com/:path*",
+      },
+    ];
+  },
   images: {
     domains: ["robohash.org", "uploadthing.com", "utfs.io"],
   },
